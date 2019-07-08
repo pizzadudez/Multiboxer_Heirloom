7bd16c5bc662c58ab5ca19c29bf41efb79250de7
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-07-08 20:42:16 +0300

    fixed chest slot not equipping; added config for frame position

---
+++ config.lua
@@ -0,0 +1,6 @@
+local addonName, addonTable = ...
+
+addonTable[1].position = {
+    left = 420,
+    down = 270,
+}
--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ end
 
 function Heirloom:MainFrame()
     local mainFrame = StdUi:Frame(UIParent, 300, 300)
-    mainFrame:SetPoint('TOPLEFT', UIParent, 'CENTER', 350, 200)
+    mainFrame:SetPoint('TOPRIGHT', UIParent, 'TOPRIGHT', - self.position.left or 0, - self.position.down or 0)
     self.mainFrame = mainFrame
 
     local setFrames = {}
@@ -31,26 +31,12 @@ function Heirloom:MainFrame()
     for setName, _ in pairs(self.heirloomSets) do
         i = i + 1
         local setFrame = StdUi:Frame(self.mainFrame, 300, 50)
-        -- create loom btn
-        local createBtn = StdUi:Button(setFrame, 60, 30, 'Create Loom')
-        createBtn:SetPoint('TOPLEFT', setFrame, 'TOPLEFT', 15, -15)
-        createBtn:SetScript('OnClick', function()
-            self.hashTable = self.hashTable or self:HashTable('demon hunter')
-            table.foreach(self.hashTable, print)
-            for itemID, createQty in pairs(self.hashTable) do
-                if createQty > 0 then 
-                    C_Heirloom.CreateHeirloom(itemID)
-                    self.hashTable[itemID] = self.hashTable[itemID] - 1
-                    break
-                end
-            end
-        end)
 
         -- equip looms btn
         local equipBtn = StdUi:Button(setFrame, 60, 30, 'Equip Looms')
-        equipBtn:SetPoint('LEFT', createBtn, 'RIGHT', 10, 0)
+        equipBtn:SetPoint('TOPRIGHT', setFrame, 'TOPRIGHT', -15, -15)
         equipBtn:SetScript('OnClick', function()
-            self.hashTable = self.hashTable or self:HashTable('demon hunter')
+            self.hashTable = self.hashTable or self:HashTable(setName)
             local equipTable = {} -- hash table for dual equipment slots
             for bag = 0, 4 do
                 for slot = 1, GetContainerNumSlots(bag) do
@@ -68,6 +54,7 @@ function Heirloom:MainFrame()
                                 equipTable[invSlots[1]] = true
                             end
                         else
+                            print(invSlots[1])
                             EquipCursorItem(invSlots[1])
                         end
                     end
@@ -75,9 +62,24 @@ function Heirloom:MainFrame()
             end
         end)
 
+        -- create loom btn
+        local createBtn = StdUi:Button(setFrame, 60, 30, 'Create Loom')
+        createBtn:SetPoint('TOPRIGHT', equipBtn, 'TOPLEFT', -10, 0)
+        createBtn:SetScript('OnClick', function()
+            self.hashTable = self.hashTable or self:HashTable(setName)
+            table.foreach(self.hashTable, print)
+            for itemID, createQty in pairs(self.hashTable) do
+                if createQty > 0 then 
+                    C_Heirloom.CreateHeirloom(itemID)
+                    self.hashTable[itemID] = self.hashTable[itemID] - 1
+                    break
+                end
+            end
+        end)
+
         -- setFrame Name
         local name = StdUi:Label(setFrame, setName, 12)
-        name:SetPoint('LEFT', equipBtn, 'RIGHT', 10, 0)
+        name:SetPoint('RIGHT', createBtn, 'LEFT', -10, 0)
 
         -- anchor setFrames
         if i == 1 then
@@ -140,6 +142,8 @@ function Heirloom:GetItemInvetorySlot(itemID)
         invSlot = {11, 12}
     elseif strfind(invType, 'TRINKET') then
         invSlot = {13, 14}
+    elseif strfind(invType, 'ROBE') then
+        invSlot = {5}
     else
         invSlot = {_G[invType:gsub('TYPE', 'SLOT')]}
     end
